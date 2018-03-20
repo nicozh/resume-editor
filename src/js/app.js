70a20c0759ee4500a3f6ba1e2efe487d8a43a3e8
zhang <mitmiky@outlook.com>
2018-03-20 23:26:33 +0800

    添加分享/打印/用户头像/项目预览图

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,31 @@
 var app = new Vue({
     el: '#app',
     data: {
+        sharingFrame: false,
         logInBt: false,
         logOutBt: false,
         logInFrame: false,
         signUpFrame: false,
         resume: {
-            name: 'xxx',
+            name: '姓名',
             gender: '男',
             age: '22',
             email: 'xxxx@outlook.com',
             phone: '15123456789',
             weChat: 'xxxx',
+            description: '简介',
+            blog: '',
+            github: '',
+            title: '前端工程师',
             skils: [
                 { name: 'HTML&CSS', description: '熟悉HTML&CSS' },
                 { name: 'vue', description: '熟悉vue' },
             ],
             projects: [
-                { name: '画板', description: '在线画板', link: 'link', code: 'github' },
+                { name: '画板', description: '在线画板', link: 'link', code: 'github', preview: 'url' },
+                { name: '佩奇', description: 'CSS3佩奇', link: 'link', code: 'github', preview: 'url' },
             ],
+            projectIndex: NaN
         },
         userData: {
             userName: '',
@@ -31,42 +38,101 @@ var app = new Vue({
         },
         currentUser: {
             userName: '',
-            id: ''
+            id: '',
+            userImg: '',
+        },
+        sharLink: {
+            link: ''
         }
+
     },
     methods: {
+        copyLink() {
+            var Url2 = document.getElementById("url");
+            Url2.select(); // 选择对象
+            document.execCommand("Copy"); // 执行浏览器复制命令
+            console.log("已复制好，可贴粘。");
+        },
         getCurrentUser() {
             let currentUser = AV.User.current()
             if (currentUser) {
+                let userId = currentUser.id
+                this.sharLink.link = location.origin + location.pathname + "?id=" + userId
                 this.currentUser.userName = currentUser.attributes.username
-                this.currentUser.id = currentUser.id
+                this.currentUser.userImg = currentUser.attributes.userImg
+                this.currentUser.id = userId
                 this.logOutBt = true
-                this.getResume()
+                this.getResume(userId)
                 // console.log(currentUser)
             } else {
                 this.logInBt = true
-                this.currentUser.userName = '请登录'
+                this.currentUser.userName = ''
                 // alert("请登录")
             }
         },
         y(key, value) {
             this.resume[key] = value
         },
+        getProjectIndex(index) {
+            this.resume.projectIndex = index
+        },
         onSkil(key, num, value, newValue) {
             this.resume[key][Number(num)][value] = newValue
         },
         addSkil() {
-            this.resume.skils.push({ name: '', description: '' })
+            this.resume.skils.push({ name: '技能', description: '描述' })
         },
         deleteSkil(index) {
-            console.log(index)
             this.resume.skils.splice(index, 1)
         },
         addProject() {
-            this.resume.projects.push({ name: '', description: '', link: '', code: '' })
+            this.resume.projects.push({ name: '项目名', description: '描述', link: '预览链接', code: '源码地址',preview:'图片' })
         },
         deleteProject(index) {
-            this.resume.projects.splice(index,1)
+            this.resume.projects.splice(index, 1)
+        },
+
+        saveImages(e) {
+            let currentId = e.currentTarget.id
+            e.preventDefault();
+            let files = e.currentTarget.files
+            if (files.length > 0) {
+                let localFile = files[0];
+                let name = 'avatar.jpg';
+                let file = new AV.File(name, localFile);
+                file.save().then((file) => {
+                    // 文件保存成功
+                    let currentUser = AV.User.current();   //获取当前登录用户
+                    var user = AV.Object.createWithoutData('User', currentUser.id);
+                    console.log('文件保存成功')
+                    let fileUrl = file.url()
+                    console.log(fileUrl)     
+                    console.log(currentId)
+                    if (currentId === 'head-img') {
+                        this.currentUser.userImg = fileUrl
+                        user.set('userImg', fileUrl);
+                    }else if(currentId ==='preview-img'){
+                        console.log(22)
+                        this.resume.projects[this.resume.projectIndex].preview=fileUrl
+                        user.set('previewImg',fileUrl );
+                    }
+                    //获取url
+                    
+                    // 储存url
+                    console.log(2)
+                    user.save().then((todo) => {
+                        console.log(3)
+                        alert('保存成功')
+                    }, (error) => {
+                        alert('保存失败')
+                        console.log(error)
+                    })
+
+                }, (error) => {
+                    // 异常处理
+                    console.error(error);
+                })
+            }
         },
         saveResume() {
             let currentUser = AV.User.current();   //判断当前用户是否登录
@@ -94,9 +160,9 @@ var app = new Vue({
             }
 
         },
-        getResume() {
+        getResume(id) {
             var user = new AV.Query('User');
-            user.get(this.currentUser.id).then((todo) => {
+            user.get(id).then((todo) => {
                 this.resume = Object.assign(this.resume, JSON.parse(todo._hashedJSON.resume))
                 // 成功获得实例
                 // todo 就是 id 为 57328ca079bc44005c2472d0 的 Todo 对象实例
@@ -114,9 +180,13 @@ var app = new Vue({
             user.setPassword(this.userData.password);
             // 设置邮箱
             user.setEmail(this.userData.email);
+            user.set('userImg', '');
+            user.set('previewImg','')
             user.signUp().then((loginedUser) => {
-                console.log(loginedUser);
+                alert('注册成功')
+                window.location.reload()
             }, (error) => {
+                console.log(error)
                 if (error.code === 202) {
                     alert('用户名已经存在,请更换用户名')
                 } else if (error.code === 203 || 214) {
@@ -126,14 +196,16 @@ var app = new Vue({
         },
         logIn() {
             AV.User.logIn(this.logInData.userName, this.logInData.password).then((loginedUser) => {
-                // 
+            
                 this.currentUser.userName = loginedUser.attributes.username
                 this.currentUser.id = loginedUser.id
-                console.log(this.currentUser.id)
+                this.currentUser.userImg = loginedUser.attributes.userImg
+                // console.log(this.currentUser.id)
                 this.logInFrame = false
                 this.logInBt = false
                 this.logOutBt = true
                 alert('登录成功')
+                // window.location.reload()
             }, (error) => {
                 if (error.code === 210) {
                     alert('用户名和密码不匹配')
@@ -151,10 +223,20 @@ var app = new Vue({
 
             // 现在的 currentUser 是 null 了
             // let currentUser = AV.User.current();
+        },
+        print() {
+            window.print()
         }
 
     }
 })
 
-window.onload = app.getCurrentUser()
+window.onload = function () {
+    let searchId = location.search.split('=')[1]
+    if (searchId) {
+        app.getResume(searchId)
+    } else {
+        app.getCurrentUser()
+    }
+}
 
