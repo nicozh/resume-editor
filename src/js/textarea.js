48fa2ffe3fef58fb0ebe1e688719554daf81d92a
zhang <mitmiky@outlook.com>
2018-03-22 23:31:39 +0800

    刷新页面输入框高度自适应

--- src/js/textarea.js
+++ src/js/textarea.js
@@ -1,8 +1,13 @@
 {
-
     $("#app").on('keyup', 'textarea', function (e) {
         let cur = e.target
         cur.style.height = 'auto';
         cur.style.height = cur.scrollHeight + "px";
     })
+    setTimeout(() => {
+        let x = document.querySelectorAll('textarea')
+        for (let i = 0; i < x.length; i++) {
+            $(x[i]).trigger('keyup', [1])
+        }
+    }, 1000);
 }
